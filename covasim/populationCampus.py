3df7652264921f4415690aa0dda7983a13e7f1a7
Soda <kjsoda@quinnipiac.edu>
2020-05-29 20:09:31 -0400

    Minor modifications to simCampus.init_people and simCampus.validate_pars;SimCampus objects can now run sims with static layers.

--- covasim/populationCampus.py
+++ covasim/populationCampus.py
@@ -2,7 +2,11 @@
 Defines functions that work analogously to the functions in the population module but geared toward the goals of campusSim
 '''
 
+import covasim.defaults as cvd
+import covasim.people as cvppl
 import covasim.utils as cvu
+import numpy as np
+import sciris as sc
 
 def make_students(sim, save_pop=False, popfile=None, verbose=None, die=True, reset=False):
     '''
@@ -20,8 +24,33 @@ def make_students(sim, save_pop=False, popfile=None, verbose=None, die=True, res
     elif sim.popdict and not reset:
         popdict = sim.popdict # Use stored one
         sim.popdict = None # Once loaded, remove
-    else:
+    elif sim['pop_type'] == 'campus':
         popdict = make_campus(sim)
+    else:
+        raise RuntimeWarning("populationCampus.make_students only supports the value \'campus\' for \'pop_type\'")
+        popdict = make_campus(sim)
+
+    # Ensure prognoses are set
+    if sim['prognoses'] is None:
+        sim['prognoses'] = cvpars.get_prognoses(sim['prog_by_age'])
+
+    # Actually create the people
+    people = cvppl.People(sim.pars, uid=popdict['uid'], age=popdict['age'], sex=popdict['sex'], contacts=popdict['contacts']) # List for storing the people
+
+    average_age = sum(popdict['age']/pop_size)
+    sc.printv(f'Created {pop_size} people, average age {average_age:0.2f} years', 2, verbose)
+
+    if save_pop:
+        if popfile is None:
+            errormsg = 'Please specify a file to save to using the popfile kwarg'
+            raise FileNotFoundError(errormsg)
+        else:
+            filepath = sc.makefilepath(filename=popfile)
+            sc.saveobj(filepath, people)
+            if verbose:
+                print(f'Saved population of type "{pop_type}" with {pop_size:n} people to {filepath}')
+
+    return people
 
 
 def make_campus(sim,sex_ratio = 0.5):
@@ -33,17 +62,22 @@ def make_campus(sim,sex_ratio = 0.5):
 
     # Handle sexes and ages
     uids           = np.arange(pop_size, dtype=cvd.default_int)
-    #TODO: Sex information should eventually be stored in sim.dorms, as residence halls are usually gender structured.
+    #TODO: Sex information should eventually be stored in sim.dorms, as residence halls are usually gender structured. Right now, the sex
+    #   data member does not do anything so it does not matter
     sexes          = np.random.binomial(1, sex_ratio, pop_size)
-    ages           = cvu.sample(**sim['age_dist'],size = pop_size)
-
+    ages           = cvu.sample(**sim.age_dist,size = pop_size)
+    layer_keys     = ['r','b','f','c']
+    contacts       = make_dorm_contacts(sim,layer_keys)
 
     # Store output
     popdict = {}
     popdict['uid'] = uids
     popdict['age'] = ages
     popdict['sex'] = sexes
+    popdict['contacts'] = contacts
+    popdict['layer_keys'] = layer_keys
 
+    return popdict
 
 
 def make_dorm_contacts(sim,layers):
--- covasim/simCampus.py
+++ covasim/simCampus.py
@@ -5,14 +5,18 @@ Defines the SimCampus class, which extends the Sim class to make it more amendab
 import covasim.sim as cvs
 import covasim.base as cvb
 import covasim.baseCampus as cvbc
+import covasim.utils as cvu
 import covasim.defaults as cvd
+import covasim.populationCampus as cvpc
+import covasim.misc as cvm
 import numpy as np
+import sciris as sc
 
 class SimCampus(cvs.Sim):
 
-    def __init__(self, pars=None, datafile=None, datacols=None, label=None, simfile=None, popfile=None, load_pop=False, save_pop=False, debug= True, **kwargs):
+    def __init__(self, pars=None, datafile=None, datacols=None, label=None, simfile=None, popfile=None, load_pop=False, save_pop=False, debug= False, **kwargs):
         super().__init__()
-        #self['pop_type'] = 'campus' #This is just bookkeeping right now
+        self['pop_type'] = 'campus' #This is just bookkeeping right now
         self.age_dist = {'dist':'uniform', 'par1':18, 'par2':22} #This new parameter provides a function for the age distribution of People objects
         self.debug = debug #This data member communicates whether the simulation is being used for software testing
 
@@ -28,6 +32,8 @@ class SimCampus(cvs.Sim):
         self['contacts']    = {'r': -1,'b': 3,'f': 5,'c': 20}
         self['iso_factor']  = {'r': 1 ,'b': 1,'f': 1,'c': 1}
         self['quar_factor'] = {'r': 1 ,'b': 1,'f': 1,'c': 1}
+        #TODO: Some layers will be dynamic in time, but we will need to update how this is handled in self.step()
+        self['dynam_layer'] = {'r': 0 ,'b': 0,'f': 0,'c': 0}
 
         self.dorm_offsets = np.array([0] * (len(self.dorms) + 1)) #This array records the first agent ID for each Dorm object in self.dorms
         self['pop_size'] = 0 #Update the population size to match the number of People in self.dorms
@@ -39,10 +45,11 @@ class SimCampus(cvs.Sim):
             counter += new
 
         self.dorm_offsets[-1] = self['pop_size'] #The population size is added as a convenience for later functions
+        #TODO: We need to find a way to update the parameters again without overwriting the instructions above
         #self.update_pars(self.pars, **kwargs)   # We have to update the parameters again in case any of the above overwrote a user provided value
 
 
-    def init_students(self, save_pop=False, load_pop=False, popfile=None, verbose=None, **kwargs):
+    def init_people(self, save_pop=False, load_pop=False, popfile=None, verbose=None, **kwargs):
         '''
         This is a modification of the superclass function.
 
@@ -63,7 +70,7 @@ class SimCampus(cvs.Sim):
             self.load_population(popfile=popfile)
 
         # Actually make the people
-        self.people = cvpop.make_people(self, save_pop=save_pop, popfile=popfile, verbose=verbose, **kwargs)
+        self.people = cvpc.make_students(self, save_pop=save_pop, popfile=popfile, verbose=verbose, **kwargs)
         self.people.initialize() # Fully initialize the people
 
         # Create the seed infections
@@ -71,3 +78,65 @@ class SimCampus(cvs.Sim):
         self.people.infect(inds=inds, layer='seed_infection')
         return
 
+
+    def validate_pars(self, validate_layers=True):
+        '''
+        This is a slight modification of the superclass function to account for alterations in this subclass.
+
+        Args:
+            validate_layers (bool): whether to validate layer parameters as well via validate_layer_pars() -- usually yes, except during initialization
+        '''
+
+        # Handle types
+        for key in ['pop_size', 'pop_infected', 'pop_size']:
+            try:
+                self[key] = int(self[key])
+            except Exception as E:
+                errormsg = f'Could not convert {key}={self[key]} of {type(self[key])} to integer'
+                raise ValueError(errormsg) from E
+
+        # Handle start day
+        start_day = self['start_day'] # Shorten
+        if start_day in [None, 0]: # Use default start day
+            start_day = '2020-03-01'
+        self['start_day'] = cvm.date(start_day)
+
+        # Handle end day and n_days
+        end_day = self['end_day']
+        n_days = self['n_days']
+        if end_day:
+            self['end_day'] = cvm.date(end_day)
+            n_days = cvm.daydiff(self['start_day'], self['end_day'])
+            if n_days <= 0:
+                errormsg = f"Number of days must be >0, but you supplied start={str(self['start_day'])} and end={str(self['end_day'])}, which gives n_days={n_days}"
+                raise ValueError(errormsg)
+            else:
+                self['n_days'] = int(n_days)
+        else:
+            if n_days:
+                self['n_days'] = int(n_days)
+                self['end_day'] = self.date(n_days) # Convert from the number of days to the end day
+            else:
+                errormsg = f'You must supply one of n_days and end_day, not "{n_days}" and "{end_day}"'
+                raise ValueError(errormsg)
+
+        # Handle population data
+        popdata_choices = ['campus']
+        choice = self['pop_type']
+        if choice and choice not in popdata_choices:
+            choicestr = ', '.join(popdata_choices)
+            errormsg = f'Population type "{choice}" not available; choices are: {choicestr}'
+            raise ValueError(errormsg)
+
+        # Handle interventions and analyzers
+        self['interventions'] = sc.promotetolist(self['interventions'], keepnone=False)
+        for i,interv in enumerate(self['interventions']):
+            if isinstance(interv, dict): # It's a dictionary representation of an intervention
+                self['interventions'][i] = cvi.InterventionDict(**interv)
+        self['analyzers'] = sc.promotetolist(self['analyzers'], keepnone=False)
+
+        # Optionally handle layer parameters
+        if validate_layers:
+            self.validate_layer_pars()
+
+        return
